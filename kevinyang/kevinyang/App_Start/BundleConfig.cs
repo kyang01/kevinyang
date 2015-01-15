e0c5481ab398675c4fd5dd0ee9ec86d1bbde7f35
Kevin <candokevin@hotmail.com>
2015-01-15 02:04:08 -0500

    Removed all the stock Windows features

--- kevinyang/kevinyang/App_Start/BundleConfig.cs
+++ kevinyang/kevinyang/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace kevinyang
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/styles.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
